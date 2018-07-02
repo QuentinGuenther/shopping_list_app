b81ea9f289283cd8b99f8ac85a8764cec8af6948
QuentinGuenther <qguenther@outlook.com>
2018-07-02 14:49:11 -0700

    :construction:Create Item Data Model

---
+++ models/Item.js
@@ -0,0 +1,30 @@
+/**
+ * Item data model.
+ * @module
+ * 
+ * @description The Item schema represents an item in a list.
+ * For example on a list with many items (milk, eggs, cheese), this model 
+ * would represent each individual item.
+ */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	quantity: {
+		type: Number,
+		default: 1
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = {
+	ItemSchema,
+	Item = mongoose.model('item', ItemSchema)
+};
