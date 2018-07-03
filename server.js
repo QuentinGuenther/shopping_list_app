af4fa3a35aa2327adc0bd2d18c533099be993662
Quentin Guenther <qguenther95@live.com>
2018-07-02 18:30:19 -0700

    :sparkles:Server Core

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// API Routes
+const items = require('./routes/api/items');
+
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+// Console.log success or error message
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Use Routes
+
+//
+// TODO: Add 404 route
+// TODO: Add 500s route
+// TODO: Add Lists route
+// TODO: Add Groups route
+// TODO: Authenticate routes.
+//
+
+app.use('/api/items', items);
+
+// Set port for server to run on
+const port = process.env.PORT || 5000;
+// Start the server and console.log the port number
+app.listen(port, () => console.log(`Server started on port ${port}`));
