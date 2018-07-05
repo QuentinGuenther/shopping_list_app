bb896bfee7a40ce1f637b8b8f01d92278ae2ae5f
Quentin Guenther <qguenther95@live.com>
2018-07-05 15:20:05 -0700

    :sparkles:Add new users to the database after google authentication

--- config/passport-google.config.js
+++ config/passport-google.config.js
@@ -2,15 +2,42 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
 const keys = require('./keys').google;
+const User = require('../models/User');
 
 // Use Google Strategy
 passport.use(
 	new GoogleStrategy({
-		// options for Google Strategy
+		// options for Google Strategy.
 		callbackURL: '/api/auth/google/redirect',
 		clientID: keys.clientID,
 		clientSecret: keys.clientSecret
-	}, () => {
-		// Passport callback function
+	}, (accessToken, refreshToken, profile, done) => {
+
+		// Check if user already exists in the database.
+		User.findOne({provider_id: profile.id})
+		.then((currentUser) => {
+			if(currentUser) {
+				// Already have user.
+				// TODO: create action for when a user is found
+			} else {
+				// Create a new user in db.
+				new User({
+					provider_id: profile.id,
+					display_name: profile.displayName,
+					name: {
+						family_name: profile.name.familyName,
+						given_name: profile.name.givenName
+					},
+					email: profile.emails[0].value,
+					gender: profile.gender,
+					provider: 'Google',
+					profile_image_url: profile._json.image.url
+				})
+				.save()
+				.then((newUser) => {
+					// TODO create action for after a user is made
+				});
+			}
+		});
 	})
 );
